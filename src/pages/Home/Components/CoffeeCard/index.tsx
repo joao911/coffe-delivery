162fa04b7ce9300e03fc0621968f54c619764dae
João Paulo Duarte <joao.sousa@tangotech.com.br>
2024-05-14 16:49:55 -0300

    feat: added coffees

--- src/pages/Home/Components/CoffeeCard/index.tsx
+++ src/pages/Home/Components/CoffeeCard/index.tsx
@@ -1,28 +1,45 @@
 import React from "react";
 import { ShoppingCart } from "phosphor-react";
+import { map } from "lodash";
 
-export const CoffeeCard: React.FC = () => {
+interface CoffeeCardProps {
+  coffee: {
+    id: number;
+    tags: string[];
+    name: string;
+    description: string;
+    photo: string;
+    price: number;
+  };
+}
+export const CoffeeCard: React.FC<CoffeeCardProps> = ({ coffee }) => {
   return (
     <div className="flex flex-col items-center justify-center pt-0 p-6 bg-base-card rounded-br-[36px] rounded-tl-[36px] rounded-tr-[6px] rounded-bl-[6px] ">
       <img
-        src="/coffees/americano.png"
+        src={`/coffees/${coffee.photo}`}
         alt="Nossos cafés"
         className="mt-[-1.25rem]"
       />
-      <div className="flex items-center justify-center py-[0.25rem] px-[0.5rem] mt-4 rounded-3xl bg-brand-yellow-light text-brand-yellow-dark mb-5">
-        <p className="text-[0.625rem] font-bold uppercase">Tradicional</p>
+      <div className="flex items-center justify-center gap-2">
+        {map(coffee.tags, (tag) => (
+          <div
+            key={tag}
+            className="flex items-center justify-center py-[0.25rem] px-[0.5rem] mt-4 rounded-3xl bg-brand-yellow-light text-brand-yellow-dark mb-5"
+          >
+            <p className="text-[0.625rem] font-bold uppercase">{tag}</p>
+          </div>
+        ))}
       </div>
-      <h1 className="font-extrabold text-title-title-m">
-        Expresso tradicional
-      </h1>
+
+      <h1 className="font-extrabold text-title-title-m">{coffee.name}</h1>
       <p className="text-base-label text-[0.875rem] mb-8">
-        O tradicional café feito com água quente e grãos moídos
+        {coffee.description}
       </p>
       <div className="flex items-center justify-between w-full gap-4">
         <div className="flex items-center gap-2">
           <p className="tex-base-text text-[0.875rem] font-normal">R$</p>
           <strong className="text-base-text text-[1.5rem]  font-extrabold">
-            9,90
+            {coffee.price.toFixed(2)}
           </strong>
         </div>
         <div className="flex items-center flex-1 gap-2 ">
--- src/pages/Home/Components/OurCoffees/index.tsx
+++ src/pages/Home/Components/OurCoffees/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { map } from "lodash";
 import ContainerItem from "../../../../components/ContainerItem";
 import { CoffeeCard } from "../CoffeeCard";
+import { coffees } from "../../../../data/coffees";
 
 const OurCoffees: React.FC = () => {
   return (
@@ -8,20 +10,9 @@ const OurCoffees: React.FC = () => {
       <section className="w-full px-10 mt-2">
         <h1 className="font-extrabold text-title-title-l">Nossos cafés</h1>
         <div className="grid md:grid-cols-4 gap-6 mt-[3.5rem]">
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
+          {map(coffees, (coffee) => (
+            <CoffeeCard key={coffee.id} coffee={coffee} />
+          ))}
         </div>
       </section>
     </ContainerItem>
